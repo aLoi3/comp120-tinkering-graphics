253869acba4eb2fff661e31f1907af588f982887
Louis Foy <louis_foy@yahoo.co.uk>
2017-10-12 15:54:10 +0100

    - Reviewed PEP-8 standard, made adjustments to comply - Added colour_close_enough - Moved functions so they don't break up main code - Replaced pixel_search parameters and fleshed out function to replace colours - Tested functionality with Hollywood-style whitewashing - Confirmed compilable!

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 import pygame
 import math
 
-pygame.init()
-picture = pygame.image.load('murica.jpg')
-screen = pygame.display.set_mode((640,480))
-
-def pixelSearch(picture, area, colour):
+def pixel_search(picture, find_colour, replacement_colour, threshold):
     pixels = pygame.PixelArray(picture)
     for x in xrange(0, picture.get_width()):
         for y in xrange(0, picture.get_height()):
-            pixelColour = picture.unmap_rgb(pixels[x, y])
+            pixel_colour = picture.unmap_rgb(pixels[x, y])
+            if colour_close_enough(pixel_colour, find_colour, threshold):
+                pixel_colour = replacement_colour
+            pixels[x, y] = picture.map_rgb(pixel_colour)
     del pixels
 
-def colourDistance(colourOne, colourTwo):
-    distance = math.sqrt(pow(colourOne.r-colourTwo.r, 2) + pow(colourOne.g-colourTwo.g, 2) + pow(colourOne.b-colourTwo.b, 2))
+
+def colour_distance(colour_one, colour_two):
+    distance = math.sqrt(pow(colour_one.r-colour_two.r, 2)
+                         + pow(colour_one.g-colour_two.g, 2)
+                         + pow(colour_one.b-colour_two.b, 2))
     return distance
+
+
+def colour_close_enough(colour_one, colour_two, threshold):
+    return colour_distance(colour_one, colour_two) < threshold
+
+pygame.init()
+picture = pygame.image.load('murica.jpg')
+picture = picture.convert(24)
+screen = pygame.display.set_mode((640, 480))
+
+# temporarily scale up the picture
+picture = pygame.transform.scale(picture, (picture.get_width() * 4, picture.get_height() * 4))
+
+# test with hollywood-style whitewashing
+pixel_search(picture, pygame.Color(105, 54, 51), pygame.Color(231, 213, 201), 40)
 
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+        if (event.type == pygame.QUIT or
+                (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
             running = False
 
     screen.blit(picture, (0, 0))

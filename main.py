c5c2d9a3257b2264231d51e975091a19e92f8f3a
Tomas Mazurkevic <mazurkevic.tomas@Gmail.com>
2017-10-12 17:39:57 +0100

    * Button is checked for position * Implemented provisional code for uploading the palette

--- main.py
+++ main.py
@@ -22,16 +22,22 @@ def colour_distance(colour_one, colour_two):
 def colour_close_enough(colour_one, colour_two, threshold):
     return colour_distance(colour_one, colour_two) < threshold
 
+
+# main thing
 pygame.init()
-picture = pygame.image.load('murica.jpg')
+picture = pygame.image.load('murica.png')
 picture = picture.convert(24)
+#palette = pygame.image.load('palette.png')
+#palette = picture.convert(24)
 screen = pygame.display.set_mode((640, 480))
+colour_under_mouse = pygame.Color (0,0,0)
+colour_on_palette = pygame.Color (255,255,255)
+
+# test with hollywood-style whitewashing
+#pixel_search(picture, pygame.Color(105, 54, 51), pygame.Color(231, 213, 201), 40)
 
 # temporarily scale up the picture
 picture = pygame.transform.scale(picture, (picture.get_width() * 4, picture.get_height() * 4))
-
-# test with hollywood-style whitewashing
-pixel_search(picture, pygame.Color(105, 54, 51), pygame.Color(231, 213, 201), 40)
 
 running = True
 while running:
@@ -39,6 +45,14 @@ while running:
         if (event.type == pygame.QUIT or
                 (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
             running = False
+        mouse_position = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            #pixel_search(picture, colour_under_mouse, pygame.Color(255, 255, 255), 20)
+            thingy = picture.get_rect()
+            if thingy.collidepoint(mouse_position):
+                colour_under_mouse = picture.get_at(mouse_position)
+
 
     screen.blit(picture, (0, 0))
+    #screen.blit(palette, (6, 36))
     pygame.display.flip()
